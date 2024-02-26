9d34835ef4be762bc37c548a7ec142d504935dc2
Shafaq-Juda <sjuda008@gmail.com>
2024-02-26 16:58:53 -0600

    created something

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -60,4 +60,9 @@ class MoviesController < ApplicationController
   def new
       render template: "movies/new"
   end
+
+  def edit
+      @the_movie = Movie.where(id: params.fetch(:id)) [0]
+      render template: "movies/edit"
+  end
 end
